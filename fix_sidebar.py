5d89d8d3ff719ff4ffd2e5f6a891061bbcd4feca
fareed-wq <fareed2official@gmail.com>
2026-08-21 21:46:36 +0300

    Clean up temporary scripts

--- fix_sidebar.py
+++
@@ -1,4 +0,0 @@
-import re
-content = open('src/components/layout/Sidebar.jsx').read()
-content = re.sub(r'title=\"([^\"]+)\"(?! aria-label)', r'title=\"\1\" aria-label=\"\1\"', content)
-open('src/components/layout/Sidebar.jsx', 'w').write(content)
